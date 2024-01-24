24acccec73c43236da43d34243a988561f902b30
djoudi-abdallah <abdellahdjoudi17@gmail.com>
2024-01-25 00:50:08 +0100

    add

--- src/component/Graphe.js
+++ src/component/Graphe.js
@@ -1,36 +1,36 @@
 import { Line } from "react-chartjs-2";
 
 const Graphe = ({ data }) => {
-    // Assuming data is an object like { 2021: 120000, 2022: 150000, ... }
-    const years = Object.keys(data);
-    const sales = Object.values(data);
-  
-    const options = {
-      responsive: true,
-      plugins: {
-        legend: {
-          position: "top",
-        },
-        title: {
-          display: true,
-          text: "Yearly Sales Data",
-        },
+  // Assuming data is an object like { 2021: 120000, 2022: 150000, ... }
+  const years = Object.keys(data);
+  const sales = Object.values(data);
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
       },
-    };
-  
-    const chartData = {
-      labels: years,
-      datasets: [
-        {
-          label: "Total Sales",
-          data: sales,
-          borderColor: "rgb(75, 192, 192)",
-          backgroundColor: "rgba(75, 192, 192, 0.5)",
-        },
-      ],
-    };
-  
-    return <Line options={options} data={chartData} />;
+      title: {
+        display: true,
+        text: "Monthly Sales Data",
+      },
+    },
   };
-  
-  export default Graphe;
+
+  const chartData = {
+    labels: years,
+    datasets: [
+      {
+        label: "Total Sales",
+        data: sales,
+        borderColor: "rgb(75, 192, 192)",
+        backgroundColor: "rgba(75, 192, 192, 0.5)",
+      },
+    ],
+  };
+
+  return <Line options={options} data={chartData} />;
+};
+
+export default Graphe;
--- src/component/PercentageCircle.js
+++ src/component/PercentageCircle.js
@@ -1,70 +1,74 @@
-import React, { useState, useEffect } from 'react';
-import { Pie } from 'react-chartjs-2';
-import 'chart.js/auto';
-import axios from 'axios'; 
+import React, { useState, useEffect, useRef } from 'react';
+import Chart from "chart.js/auto";
+import axios from 'axios';
+import { useLocation } from "react-router-dom";
 
 const PieChart = () => {
+  const chartRef = useRef(null);
   const [salesData, setSalesData] = useState([]);
+  const [products, setProducts] = useState([]);
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
 
   useEffect(() => {
-    const fetchSalesData = async () => {
+    const fetchSalesmontant = async () => {
+      
       try {
-        const response = await axios.get('http://localhost:3001/dashbord/circle');
-        setSalesData(response.data);
+        const response = await axios.get(
+          `http://localhost:3001/dashbord/circle`
+        );
+        setSalesData(response.data.totalSales);
+        setProducts(response.data.centreCodes);
       } catch (error) {
-        console.error('Erreur lors de la récupération des données de vente', error);
+        console.error("Error fetching:", error);
       }
     };
 
-    fetchSalesData();
+    fetchSalesmontant();
   }, []);
-  const backgroundColors = [
-    'rgba(255, 99, 132, 0.2)', 
-    'rgba(54, 162, 235, 0.2)',   
-    'rgba(255, 206, 86, 0.2)',   
-    'rgba(75, 192, 192, 0.2)',   
-    'rgba(153, 102, 255, 0.2)',  
 
-  ];
-  
-  const borderColors = [
-    'rgba(255, 99, 132, 1)',  
-    'rgba(54, 162, 235, 1)',   
-    'rgba(255, 206, 86, 1)',  
-    'rgba(75, 192, 192, 1)',   
-    'rgba(153, 102, 255, 1)',  
-    
-  ];
-  
+  useEffect(() => {
+    if (products.length && salesData.length) {
+      const ctx = chartRef.current.getContext("2d");
 
-  const chartData = {
-    labels: salesData.map(item => item.centreCode),
-    datasets: [
-      {
-        label: 'Total des ventes',
-        data: salesData.map(item => item.totalSales),
-        backgroundColor: backgroundColors,
-        borderColor: borderColors,
-        borderWidth: 1,
-      },
-    ],
-  };
-
-  const options = {
-    plugins: {
-      legend: {
-        display: false 
+      if (chartRef.current.chart) {
+        chartRef.current.chart.destroy();
       }
+
+      chartRef.current.chart = new Chart(ctx, {
+        type: "pie",
+        data: {
+          labels: products,
+          datasets: [
+            {
+              data: salesData,
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.6)",
+                "rgba(54, 162, 235, 0.6)",
+                "rgba(255, 206, 86, 0.6)",
+                "rgba(75, 192, 192, 0.6)",
+                "rgba(153, 102, 255, 0.6)",
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        },
+      });
     }
-  };
-  
-  
+  }, [salesData, products]);
+
   return (
-    <div className="flex justify-center items-center w-[85%]">
-      <Pie data={chartData} options={options} />
+    <div style={{ width: "100%" }}>
+      <canvas id="pieChart" ref={chartRef}></canvas>
     </div>
   );
-  
 };
-  export default PieChart;
 
+export default PieChart;
